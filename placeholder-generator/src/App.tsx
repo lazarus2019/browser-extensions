65c85884d41158654c774989c019cf3b30815030
Thai Son <thaisonbk2020@gmail.com>
2024-05-29 01:21:57 +0700

    feat: define gitignore file for all the extensions

--- placeholder-generator/src/App.tsx
+++ placeholder-generator/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [color, setColor] = useState('#fff');
+  const [color, setColor] = useState('#ffffff');
   const [isExpand, setIsExpand] = useState(false);
 
   const handleOnClick = async () => {
