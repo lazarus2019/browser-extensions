bbc15e28da8ca442299df742a0902d4cc171998a
Thai Son <thaisonbk2020@gmail.com>
2024-05-29 01:21:12 +0700

    feat(extension): placeholer image generator

---
+++ placeholder-generator/src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('#fff');
+  const [isExpand, setIsExpand] = useState(false);
+
+  const handleOnClick = async () => {
+    const [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id! },
+      args: [color],
+      func: (color) => {
+        // alert('Hellow from my vite exteions');
+        document.body.style.backgroundColor = color;
+      },
+    });
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          height: isExpand ? '400px' : '200px',
+        }}
+      >
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <input
+        type="color"
+        onChange={(e) => setColor(e.currentTarget.value)}
+        value={color}
+      />
+
+      <div className="card">
+        <button onClick={() => setIsExpand((prev) => !prev)}>
+          Toggle expand popup
+        </button>
+        <button onClick={handleOnClick}>Show alert</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
